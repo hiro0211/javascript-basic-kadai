cfd6e83325bbb60279bf414850ac854ecab9968b
hiro0211 <143386971+hiro0211@users.noreply.github.com>
2023-09-09 21:31:19 +0900

    課題7をプッシュ

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+let num = 15;
+
+if (num === 3 || num === 6 || num === 9) {
+  console.log("3の倍数です")
+}
+else if ( num === 5 || num === 10 ) {
+  console.log("5の倍数です")
+}
+else if (num === 15) {
+  console.log("3と5の倍数です")
+}
+else {
+  console.log(num)
+}
